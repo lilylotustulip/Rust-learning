e9574e0a7cd4d434fe9700898be91f8f94cea051
lotus <lilylotustulip@gmail.com>
2026-07-23 16:55:14 +0100

    small exercice to practice

---
+++ exercices/temp_converter/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let celcius: f64 = 27.0;
+    let fahrenheit: f64 = (celcius * 1.8) + 32.0;
+
+    println!("temp in celcius is: {celcius}, and temp in fahrenheit is: {fahrenheit}");
+}
